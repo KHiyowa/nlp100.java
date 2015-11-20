302afb05e1e2f3f6833fdab26e74683a42f2c3a9
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2015-11-21 03:20:03 +0900

    Soved Q00

---
+++ src/nlp100/java/Q00.java
@@ -0,0 +1,11 @@
+package nlp100.java;
+
+public class Q00 {
+
+	public static void main(String[] args) {
+		StringBuffer str = new StringBuffer();
+		str.append("stressed");
+		System.out.println(str.reverse().toString());
+	}
+	
+}
