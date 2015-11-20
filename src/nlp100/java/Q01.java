718316bc1284b1c571e9247560194ad2628f967a
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2015-11-21 03:20:08 +0900

    Soved Q01

---
+++ src/nlp100/java/Q01.java
@@ -0,0 +1,12 @@
+package nlp100.java;
+
+public class Q01 {
+
+	public static void main(String[] args) {
+		String str = "パタトクカシーー";
+		for (int i : new int[] {1, 3, 5, 7}) {
+			System.out.print(str.charAt(i));
+		}
+	}
+
+}
